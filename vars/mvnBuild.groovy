c9623f269885aabd37501bb531aad31706ef0e59
Hesham Mohamed Abdelhay Khalil <hesham3eg@gmail.com>
2025-05-27 01:11:38 +0300

    using Jpipeline

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -2,8 +2,8 @@ def call(String skipTest = 'true') {
     def mvnHome = tool name: 'maven-3.8', type: 'hudson.tasks.Maven$MavenInstallation'
     withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
         dir('java-jenkins-sample-project') {  // adjust to your actual folder name
-            sh "find . -name pom.xml"
             sh "mvn clean package install -Dmaven.test.skip=${skipTest}"
+            sh "find . -name pom.xml"
         }
     }
 }
