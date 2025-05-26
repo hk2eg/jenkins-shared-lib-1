1777e822ccc7bed98e24882ecee206da3a1c9c24
Hesham Mohamed Abdelhay Khalil <hesham3eg@gmail.com>
2025-05-27 02:34:59 +0300

    using Jpipeline

--- vars/Pypipeline.groovy
+++ vars/Pypipeline.groovy
@@ -1,6 +1,4 @@
-// vars/pypipeline.groovy
 def call(Map config = [:]) {
-  // allow override of node label, defaults, etc.
   String nodeLabel   = config.nodeLabel   ?: 'agent-1'
   String image       = config.image       ?: 'hk2802/flask-sample'
   String credentials = config.credentials ?: 'docker-hub-login'
@@ -15,13 +13,12 @@ def call(Map config = [:]) {
         usernameVariable: 'DOCKER_USR',
         passwordVariable: 'DOCKER_PSW'
     )]) {
-      // Build
+        
       stage('Build Docker Image') {
         dockerLogin(env.DOCKER_USR, env.DOCKER_PSW)
         dockerBuild(image, "v${env.BUILD_NUMBER}")
       }
 
-      // Push
       stage('Push Docker Image') {
         dockerLogin(env.DOCKER_USR, env.DOCKER_PSW)
         dockerPush(image, "v${env.BUILD_NUMBER}")
