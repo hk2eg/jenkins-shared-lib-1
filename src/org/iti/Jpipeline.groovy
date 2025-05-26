3580ed2b375a6080b3119d4e3e1b578232cb3732
Hesham Mohamed Abdelhay Khalil <hesham3eg@gmail.com>
2025-05-27 00:56:47 +0300

    using Jpipeline

--- src/org/iti/Jpipeline.groovy
+++ src/org/iti/Jpipeline.groovy
@@ -1,6 +1,6 @@
 package org.iti
 
-def run(Map args = [:]) {
+def executePipeline(Map args = [:]) {
     def script = args.script
     def docker_user = args.docker_user
     def docker_pass = args.docker_pass
